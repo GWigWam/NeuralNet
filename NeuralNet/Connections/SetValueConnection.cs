367b6e4c542a4834c78935cb97262c59f5b5ff1f
GWigWam <welwisam@gmail.com>
2015-10-19 19:28:15 +0200

    Added some debugging displays

--- NeuralNet/Connections/SetValueConnection.cs
+++ NeuralNet/Connections/SetValueConnection.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NeuralNet.Connections {
 
+    [DebuggerDisplay("SetValueConnection out: {Output}")]
     public class SetValueConnection : Connection {
 
         public float Value {
--- NeuralNet/Connections/WeightedConnection.cs
+++ NeuralNet/Connections/WeightedConnection.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NeuralNet.Connections {
 
+    [DebuggerDisplay("WeightedConnection out: {Output}")]
     public class WeightedConnection : Connection {
 
         public float Weight {
