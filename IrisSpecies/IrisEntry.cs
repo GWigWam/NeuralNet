916bbd18fddf8964638e225abbaa3abe5c467f7c
GWigWam <welwisam@gmail.com>
2015-10-24 17:53:50 +0200

    Iris species recognizer now trains a network
    Best result so far 87% correct predictions
    
    Also added some helpfull docs about backpropagation algorithm

--- IrisSpecies/IrisEntry.cs
+++ IrisSpecies/IrisEntry.cs
@@ -10,12 +10,15 @@ namespace IrisSpecies {
         Setosa, Versicolor, Virginica
     }
 
-    internal struct IrisEntry {
+    internal class IrisEntry {
         public float SepalLength;
         public float SepalWidth;
         public float PetalLength;
         public float PetalWidth;
         public IrisSpecies Species;
+
+        public float[] AsInput => new float[] { SepalLength, SepalWidth, PetalLength, PetalWidth };
+        public float[] AsOutput => (Species == IrisSpecies.Setosa) ? new float[] { 1, 0, 0 } : (Species == IrisSpecies.Versicolor) ? new float[] { 0, 1, 0 } : new float[] { 0, 0, 1 };
 
         public IrisEntry(float sl, float sw, float pl, float pw, IrisSpecies species) {
             SepalLength = sl;
@@ -23,6 +26,32 @@ namespace IrisSpecies {
             PetalLength = pl;
             PetalWidth = pw;
             Species = species;
+        }
+
+        public static IrisSpecies SpeciesFromOutput(float[] output) {
+            float maxVal = int.MinValue;
+            int maxIndex = -1;
+            for(int i = 0; i < output.Length; i++) {
+                var curOutp = output[i];
+                if(curOutp > maxVal) {
+                    maxVal = curOutp;
+                    maxIndex = i;
+                }
+            }
+
+            switch(maxIndex) {
+                case 0:
+                return IrisSpecies.Setosa;
+
+                case 1:
+                return IrisSpecies.Versicolor;
+
+                case 2:
+                return IrisSpecies.Virginica;
+
+                default:
+                throw new Exception();
+            }
         }
     }
 }
--- IrisSpecies/Program.cs
+++ IrisSpecies/Program.cs
@@ -5,31 +5,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static NeuralNet.MathHelper;
 
 namespace IrisSpecies {
 
     internal class Program {
         private const int TrainSetSize = 130;
+        private static Random random = new Random();
 
         private static void Main(string[] args) {
-            IrisEntry[] data = DataReader.ReadFromFile("data/iris.data");
-
-            var train = data.Take(TrainSetSize);
-            var validation = data.Skip(TrainSetSize);
+            var data = DataReader.ReadFromFile("data/iris.data").OrderBy(i => random.Next());
+            foreach(var entry in data) {
+                //TODO: it is suboptimal to calculate min and max every time
+                entry.PetalLength = Normalize(entry.PetalLength, data.Min(i => i.PetalLength), data.Max(i => i.PetalLength));
+                entry.PetalWidth = Normalize(entry.PetalWidth, data.Min(i => i.PetalWidth), data.Max(i => i.PetalWidth));
+                entry.SepalLength = Normalize(entry.SepalLength, data.Min(i => i.SepalLength), data.Max(i => i.SepalLength));
+                entry.SepalWidth = Normalize(entry.SepalWidth, data.Min(i => i.SepalWidth), data.Max(i => i.SepalWidth));
+            }
 
             var network = new Network(new NeuralNet.TransferFunctions.SigmoidFunction(), true);
             network.FillNetwork(4, 3, 100);
-            network.RandomizeWeights();
 
-            foreach(var entry in train) {
-                var input = new float[] { entry.SepalLength, entry.SepalWidth, entry.PetalLength, entry.PetalWidth };
-                var output = (entry.Species == IrisSpecies.Setosa) ?
-                    new float[] { 1, 0, 0 } : (entry.Species == IrisSpecies.Versicolor) ? new float[] { 0, 1, 0 } : new float[] { 0, 0, 1 };
+            var train = data.Take(TrainSetSize).ToArray();
+            var inputAndExpectedResuls = train.Select(entry => new InputExpectedResult(entry.AsInput, entry.AsOutput));
+            var validation = data.Skip(TrainSetSize).ToArray();
 
-                var ipr = new InputResultPair(input, output);
+            var bp = new Backpropagate(network, inputAndExpectedResuls.ToArray(), 0.5f);
 
-                var res = network.GetInputResult(entry.SepalLength, entry.SepalWidth, entry.PetalLength, entry.PetalWidth);
+            int trains = 0;
+            float percentCorrect = 0;
+            while(percentCorrect <= 90) {
+                trains++;
+                bp.Train();
+
+                int success = 0;
+                foreach(IrisEntry validate in validation) {
+                    var curOutp = network.GetInputResult(validate.AsInput);
+                    IrisSpecies result = IrisEntry.SpeciesFromOutput(curOutp);
+
+                    if(result == validate.Species) {
+                        success++;
+                    }
+                }
+                percentCorrect = 100f * success / validation.Length;
+                Console.WriteLine($"#{trains} {percentCorrect}% correct");
             }
+
+            Console.WriteLine("Done training");
+            int doneSucces = 0;
+            foreach(IrisEntry entry in data) {
+                var curOutp = network.GetInputResult(entry.AsInput);
+                IrisSpecies result = IrisEntry.SpeciesFromOutput(curOutp);
+
+                if(result == entry.Species) {
+                    doneSucces++;
+                }
+            }
+
+            var percentDoneCorrect = 100 * doneSucces / data.Count();
+            Console.WriteLine($" {doneSucces}/{data.Count()} {percentDoneCorrect}% correct");
+
+            Console.ReadKey();
         }
     }
 }
