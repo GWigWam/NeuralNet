4094937eb412ec55247b01493f77e75897c8da5c
GWigWam <welwisam@gmail.com>
2015-10-27 19:36:32 +0100

    Start Handwriting prj

---
+++ Handwriting/ImageReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Handwriting {
+
+    public static class ImageReader {
+        private static Regex FileFormat;
+
+        static ImageReader() {
+            FileFormat = new Regex(@"_(?<char>[^_]+)_[A-Z0-9]+?.bmp$");
+        }
+
+        public static void ReadFromDir(string dirLoc, bool onlyNumbers = true) {
+            foreach(FileInfo file in new DirectoryInfo(dirLoc).GetFiles("*.bmp", SearchOption.AllDirectories)) {
+                string readChar = FileFormat.Match(file.Name).Groups["char"]?.Value;
+                char curChar;
+                if(char.TryParse(readChar, out curChar) && (!onlyNumbers || char.IsNumber(curChar))) {
+                }
+            }
+        }
+    }
+}
---
+++ Handwriting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handwriting {
+
+    public class Program {
+        private const string dirLoc = "F:/Zooi/github/NeuralNet/Handwriting/data/img";
+
+        private static void Main(string[] args) {
+            ImageReader.ReadFromDir(dirLoc);
+        }
+    }
+}
