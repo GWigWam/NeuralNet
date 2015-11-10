9efec768f1f6f68c7a0824c17409242df5beea37
GWigWam <welwisam@gmail.com>
2015-11-10 15:31:47 +0100

    Added input-validation

--- HandwritingGui/MainWindow.xaml.cs
+++ HandwritingGui/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -39,6 +40,10 @@ namespace HandwritingGui {
             var backBrush = background != null ? new SolidColorBrush(background.Value) : Rtb_Out.Background;
 
             Output.Add(new Paragraph(new Run(msg) { Foreground = foreBrush, Background = backBrush }));
+
+            if(!Keyboard.IsKeyToggled(Key.Scroll)) {
+                Rtb_Out.ScrollToEnd();
+            }
         }
 
         private void PreviewUnsignedIntTb(object sender, TextCompositionEventArgs e) {
@@ -49,10 +54,73 @@ namespace HandwritingGui {
             e.Handled = !UDoubleRegex.IsMatch(e.Text);
         }
 
-        private void Bt_SetupReady_Click(object sender, RoutedEventArgs e) {
+        private void Bt_ImgPath_Browse_Click(object sender, RoutedEventArgs e) {
+            var folderDial = new System.Windows.Forms.FolderBrowserDialog() {
+                ShowNewFolderButton = false
+            };
+            if(folderDial.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                Tb_ImgPath.Text = folderDial.SelectedPath;
+            }
         }
 
-        private void Bt_ImgPath_Browse_Click(object sender, RoutedEventArgs e) {
+        private void Bt_SetupReady_Click(object sender, RoutedEventArgs e) {
+            int imgDim;
+            if(!int.TryParse(Tb_ImgDimensions.Text, out imgDim) || imgDim <= 1) {
+                Log("Invalid image dimentions", Colors.Red);
+                return;
+            }
+
+            double learningRate;
+            if(!double.TryParse(Tb_LearnRate.Text, out learningRate) || learningRate <= 0) {
+                Log("Invalid learning rate", Colors.Red);
+                return;
+            }
+
+            int microBatchsize;
+            if(!int.TryParse(Tb_MicroBatchSize.Text, out microBatchsize) || microBatchsize <= 0) {
+                Log("Invalid Microbatch size", Colors.Red);
+                return;
+            }
+
+            int loadingBatchsize;
+            if(!int.TryParse(Tb_LoadBatchSize.Text, out loadingBatchsize) || loadingBatchsize <= 0 || loadingBatchsize < microBatchsize) {
+                Log("Invalid Loadingbatch size", Colors.Red);
+                return;
+            }
+
+            if(!Directory.Exists(Tb_ImgPath.Text)) {
+                Log("Invalid image folder path", Colors.Red);
+                return;
+            }
+
+            var match = new Regex(@"^(?<inp>(\d|X)+)\*(?<hid>(\d|X)+\*)+(?<out>(\d|X)+)$").Match(Tb_NetworkDimensions.Text);
+            if(!match.Success) {
+                Log("Invalid network dimension, invalid format", Colors.Red);
+                return;
+            }
+            if(match.Groups?["inp"].Value != Math.Pow(imgDim, 2).ToString()) {
+                Log("Invalid network dimension: Input", Colors.Red);
+                return;
+            }
+            int expectedOutputNr = Rb_Charset_Alphabetic.IsChecked.Value ? 26 : Rb_Charset_Digits.IsChecked.Value ? 10 : 36;
+            if(match.Groups?["out"].Value != expectedOutputNr.ToString()) {
+                Log("Invalid network dimension: Output", Colors.Red);
+                return;
+            }
+
+            int?[] hiddenHeights = match.Groups?["hid"].Value.TrimEnd('*').Split('*').Select<string, int?>(h => {
+                int outp;
+                if(int.TryParse(h, out outp))
+                    return outp;
+                return null;
+            }).ToArray();
+
+            if(hiddenHeights == null || hiddenHeights.Length == 0 || hiddenHeights.Any(i => i == null)) {
+                Log("Invalid network dimension: Hidden", Colors.Red);
+                return;
+            }
+
+            Log("All input seems to be valid", Colors.White, Colors.Green);
         }
 
         private void Tb_ImgDimensions_TextChanged(object sender, TextChangedEventArgs e) {
