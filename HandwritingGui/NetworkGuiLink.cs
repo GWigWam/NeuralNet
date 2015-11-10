44fdb93839ed0afaad6e907c8172444dfb90c684
GWigWam <welwisam@gmail.com>
2015-11-10 16:30:32 +0100

    NetworkGuiLink
    Should handle the interaction between visuals in WPF and the
    network/training

---
+++ HandwritingGui/NetworkGuiLink.cs
@@ -0,0 +1,42 @@
+﻿using NeuralNet;
+using NeuralNet.BackpropagationTraining;
+using NeuralNet.TransferFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandwritingGui {
+
+    internal class NetworkGuiLink {
+        private LazyTrainImgLoader ImgLoader;
+        private TransferFunction Transfer;
+        private Network Network;
+        private Backpropagate BackpropTrain;
+
+        public NetworkGuiLink() {
+        }
+
+        public void Init(int imgDim, double learnRate, int microBatchsize, int loadBatchsize, string imgFolder, TransferFunctionType funcType, int inputHeight, int outputHeight, int[] hiddenHeights) {
+            switch(funcType) {
+                case TransferFunctionType.Sigmoid:
+                Transfer = new SigmoidFunction();
+                break;
+
+                case TransferFunctionType.HyperbolicTangent:
+                Transfer = new HyperbolicTangentFunction();
+                break;
+            }
+
+            ImgLoader = new LazyTrainImgLoader(imgFolder, Transfer, true, true, imgDim, loadBatchsize/*, TODO: Support non-digits*/);
+            InitNetwork(learnRate, microBatchsize, inputHeight, outputHeight, hiddenHeights);
+        }
+
+        private void InitNetwork(double learnRate, int microBatchsize, int inputHeight, int outputHeight, int[] hiddenHeights) {
+            Network = new Network(Transfer, true);
+            Network.FillNetwork(inputHeight, outputHeight, hiddenHeights);
+            BackpropTrain = new Backpropagate(Network, learnRate, microBatchsize);
+        }
+    }
+}
